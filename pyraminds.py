0f93329d8249c326136edbd6395ed95fbe3b07ef
Rajasekar Munuswamy <rajasekar.unix@yahoo.com>
2023-04-09 16:53:58 -0700

    Create pyraminds.py

---
+++ pyraminds.py
@@ -0,0 +1,10 @@
+# Printing one sided pyramids using recursion
+def pyramid(n):
+    if n == 0:
+        return;
+    else:
+        print("*" * n); # Prints Pyramid bottom to up
+        pyramid(n-1);
+        print("*"*n); # Prints Pyramid up to bottom
+
+pyramid(6);
