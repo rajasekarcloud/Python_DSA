6716d17975a7735d59a6fc78aabda15d06c75df8
Rajasekar Munuswamy <rajasekar.unix@yahoo.com>
2023-05-02 07:55:46 -0700

    Update kids_math_quiz.py

--- kids_math_quiz.py
+++ kids_math_quiz.py
@@ -1,26 +1,48 @@
 import random;
 import os;
 import time;
-
+# For text to speech https://pypi.org/project/pyttsx3/
+import pyttsx3;
+engine = pyttsx3.init();
 def add_fn(count):
+    correct_ans = 0;
     for i in range(count):
         first_num = random.randint(50,99);
         sec_num = random.randint(50,99);
-        print("What is ",first_num, " + ",sec_num,"?");
+        print("\nWhat is ",first_num, " + ",sec_num,"?");
         time.sleep(10);
-        print("Time UP. Enter Any Key To Continue");
-        input("");
-        print("Answer is ",first_num+sec_num);
+        ans = int(input("Enter The Answer: "));
+        if ans == (first_num+sec_num):
+            print("\nYou Are Correct");
+            correct_ans = correct_ans + 1;
+        else:
+            print("\nYou Are Wrong. Its ",first_num+sec_num);
+    engine.say("Please find your scores.")
+    print("Total Questions: ", count);
+    print("Correct Answered: ",correct_ans);
+    print("Average Score: ",(100*correct_ans)/count);
+    engine.runAndWait()
+    engine.stop()
 
 def sub_fn(count):
+    correct_ans = 0;
     for i in range(count):
         first_num = random.randint(50, 99);
-        sec_num = random.randint(10, 50);
-        print("What is ",first_num, " - ",sec_num,"?");
+        sec_num = random.randint(1, 49);
+        print("\nWhat is ", first_num, " - ", sec_num, "?");
         time.sleep(10);
-        print("Time UP. Enter Any Key To Continue");
-        input("");
-        print("Answer is ", first_num - sec_num);
+        ans = int(input("Enter The Answer: "));
+        if ans == (first_num + sec_num):
+            print("\nYou Are Correct");
+            correct_ans = correct_ans + 1;
+        else:
+            print("\nYou Are Wrong. Its ", first_num - sec_num);
+    engine.say("Please find your scores.")
+    print("Total Questions: ", count);
+    print("Correct Answered: ", correct_ans);
+    print("Average Score: ", (100 * correct_ans) / count);
+    engine.runAndWait()
+    engine.stop()
 
 print("1. Addition");
 print("2. Subtraction");
@@ -36,5 +58,3 @@ elif option == 2:
     sub_fn(count);
 else:
     exit();
-
-
